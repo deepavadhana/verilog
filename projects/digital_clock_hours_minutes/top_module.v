6445b983c79f9f50679c93ebb6c2d7710c26a67e
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2025-06-29 22:46:09 +0530

    Create top_module.v

---
+++ projects/digital_clock_hours_minutes/top_module.v
@@ -0,0 +1,14 @@
+module top_module(
+  input clk_50Mhz,
+  input rst,
+  output wire [7:0] seg,
+  output wire [3:0] out
+);
+  wire clk_1hz,clk_500hz;
+ clk_divider dut1(.clk_50Mhz(clk_50Mhz),
+    .rst(rst),
+    .clk_1hz(clk_1hz),
+    .clk_500hz(clk_500hz)
+  );
+  seg_counter dut2( .clk_1hz(clk_1hz),.clk_50Mhz(clk_50Mhz),.rst(rst), .out(out), .seg(seg));
+endmodule
