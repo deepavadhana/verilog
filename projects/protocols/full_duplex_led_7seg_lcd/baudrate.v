8a3b8883123a730d62630543cc086ecc50d263c4
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2025-07-13 10:07:54 +0530

    Create baudrate.v

---
+++ projects/protocols/full_duplex_led_7seg_lcd/baudrate.v
@@ -0,0 +1,19 @@
+module baudrate #(
+  parameter M=5,
+  parameter N=27
+)(
+  input clk_50Mhz,
+  input rst,
+  output tick);
+  reg [M-1:0] counter;
+  wire [M-1:0] next;
+  always@(posedge clk_50Mhz or posedge rst)begin
+    if(rst)
+      counter<=0;
+      else 
+        counter<=next;
+    end
+  assign next=((counter==(N-1))?0:counter+1);
+  assign tick=((counter==(N-1))?1'b1:1'b0);
+endmodule
+  
