7b458a4bfbd2d7c307a71593821ad3848629b0e6
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2025-12-21 17:40:22 +0530

    Create instr_mem.v

---
+++ projects/risc_v/instr_mem.v
@@ -0,0 +1,11 @@
+module instr_mem #(parameter DATA_WIDTH = 32, ADDR_WIDTH = 32, MEM_SIZE = 512) (
+    input       [ADDR_WIDTH-1:0] instr_addr,
+    output      [DATA_WIDTH-1:0] instr
+);
+reg [DATA_WIDTH-1:0] instr_ram [0:MEM_SIZE-1];
+  initial begin
+    $readmemh("rv32i_book.hex", instr_ram);
+   end
+assign instr = instr_ram[instr_addr[31:2]];
+
+endmodule
