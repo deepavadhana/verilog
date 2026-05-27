2b00bd49f8370ca19144245121a1e765de9a6551
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-05-27 17:06:10 +0530

    Create array_unpacked.sv

---
+++ system_verilog/main_sv_codes/array_unpacked.sv
@@ -0,0 +1,11 @@
+// Code your design here
+
+module arr_unpacked;
+  bit arr [4:0][2:0]='{'{5'b10101,5'b10001,5'b10010},'{5'b11111,5'b10000,5'b00001},'{5'b10101,5'b10001,5'b10010},'{5'b11111,5'b10000,5'b00001},'{5'b11111,5'b10000,5'b00001}};
+  initial begin
+    foreach(arr[i,j])begin
+      $display("Array elements: arr[%0d][%0d]=%0d",i,j,arr[i][j]);
+    end
+  end
+    
+endmodule
