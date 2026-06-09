6550d8b33d0222dfb2464bb30383f5eeb6d08b08
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-06-09 17:29:12 +0530

    Create integer.sv

---
+++ system_verilog/main_sv_codes/datatypes/integer.sv
@@ -0,0 +1,17 @@
+module data_types;
+  integer a;
+ 
+  initial begin
+    $display("Before initialisation :%0b",a);
+   
+    a=32'b1010_1111_0101_0101_01x0_10z1_0xxx_xz0z;
+    
+    $display("After initialisation :%0b",a);
+   
+    a=1345260;
+   
+    $display("After initialisation :%0d",a);
+    
+   
+  end 
+endmodule
