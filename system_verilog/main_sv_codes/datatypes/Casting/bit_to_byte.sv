a19dc2fd92a30e0da670e8b5343ea37cd348f886
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-06-10 12:39:04 +0530

    Create bit_to_byte.sv

---
+++ system_verilog/main_sv_codes/datatypes/Casting/bit_to_byte.sv
@@ -0,0 +1,13 @@
+module casting_datatype;
+  bit [3:0] data;
+  byte bdata;
+  initial begin
+    data=4'b1010;
+    $display("before casting : %0b",data);
+    $display("size After casting : %0d",$size(data));
+    bdata=byte'(data);
+    $display("After casting : %0b",bdata);
+    $display("size After casting : %0d",$size(bdata));
+  end
+endmodule
+    
