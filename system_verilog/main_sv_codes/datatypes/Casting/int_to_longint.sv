3aa238826c83b26fc8b4d96ecda2bae05e178b68
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-06-10 12:41:35 +0530

    Create int_to_longint.sv

---
+++ system_verilog/main_sv_codes/datatypes/Casting/int_to_longint.sv
@@ -0,0 +1,15 @@
+// Code your testbench here
+// or browse Examples
+module casting_datatype;
+  bit [3:0] data;
+  byte bdata;
+  initial begin
+    data=4'b1010;
+    $display("before casting : %0b",data);
+    $display("size After casting : %0d",$size(data));
+    bdata=byte'(data);
+    $display("After casting : %0b",bdata);
+    $display("size After casting : %0d",$size(bdata));
+  end
+endmodule
+    
