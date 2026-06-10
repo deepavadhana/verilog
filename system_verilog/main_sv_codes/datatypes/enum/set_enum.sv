e5a90796a6a2b59c5247fe9406e076cb82b0e7aa
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-06-10 10:13:22 +0530

    Update set_enum.sv

--- system_verilog/main_sv_codes/datatypes/enum/set_enum.sv
+++ system_verilog/main_sv_codes/datatypes/enum/set_enum.sv
@@ -1,1 +1,11 @@
 
+module set_value_enum;
+  enum {Sun=0,Mon,Tue,Wed=6,Thur,Fri=9,Sat}days;
+initial begin
+  days=days.first;
+  for(int i=0;i<11;i++)begin
+  $display("DAYS:%0s,DAYS_VALUE=%0d",days.name,days);
+  days=days.next;
+  end
+end
+endmodule
