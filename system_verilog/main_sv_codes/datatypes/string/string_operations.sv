75d67dc207c380a0c04611e9e668a376e8ba6c27
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-06-10 12:09:04 +0530

    Create string_operations.sv

---
+++ system_verilog/main_sv_codes/datatypes/string/string_operations.sv
@@ -0,0 +1,25 @@
+module string_operations;
+  string str1= "DEEPAvadhana";
+  string str2="deepavadhana";
+  initial begin
+    if (str1==str2)
+      $display("Both string are equal");
+    else
+      $display("Both string are unequal");
+    if(str1!==str2)
+      $display("Both string are equal");
+    else
+      $display("Both string are unequal");
+    if(str1>str2)
+      $display("String1 is greater");
+    if(str1<str2)
+      $display("String2 is greater");
+    if(str1>=str2)
+      $display("String1 is equal or greater");
+    if(str1<=str2)
+      $display("String2 is equal or greater");
+    $display("Concatenate both strings :%0s",{str1,str2});
+    $display("Replication of str1 :%0s",{2{str1}});
+  end
+endmodule
+    
