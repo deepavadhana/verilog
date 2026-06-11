d7732caaa0b15b01c8d758b1341edf8f9cfa4b96
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-06-11 10:11:40 +0530

    Update single_unpacked.sv

--- system_verilog/main_sv_codes/datatypes/arrays/unpacked/single_unpacked.sv
+++ system_verilog/main_sv_codes/datatypes/arrays/unpacked/single_unpacked.sv
@@ -3,11 +3,11 @@ module single_unpacked;
   int b[15];
   byte c[7];
   initial begin
-    a='{4,5,0};
+    a='{4,5,0}; //only the lsb bit is returned as bit is 1 bit
     foreach(b[i])begin
       b[i]=$urandom_range(6,16);
     end
-    c='{3,4,5,6,784,1,16};
+    c='{3,4,5,6,784,1,16};//here the 784 is coverted to binary and only 8 bit fom lsb is returned as we use byte
     $display("Array elements:%p %p %p",a,b,c);
     foreach(a[i])
       $display("Array elements[%0d] : [%0d]",i,a[i]);  
