a25936302b67dd1f864398e05c153aad62f60bf6
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-06-11 11:02:13 +0530

    Update dynamic_unbound.sv

--- system_verilog/main_sv_codes/datatypes/arrays/dynamic/dynamic_unbound.sv
+++ system_verilog/main_sv_codes/datatypes/arrays/dynamic/dynamic_unbound.sv
@@ -1,1 +1,14 @@
-
+module dynamic_arr_unbound;
+  byte arr[];
+  initial begin
+    arr=new[6];
+    arr='{2,5,6,7,8};
+    $display("Array elements:%p",arr);
+    foreach(arr[i]) begin
+      $display("Array elements[%0d]:%0d",i,arr[i]);
+    end
+  $display("Size of Array elements:%0d",arr.size());
+    $display("Array element at index:%0d",arr[3]);
+    $display("Array element at index:%0d",arr[100]);//unbounded index
+  end
+endmodule
