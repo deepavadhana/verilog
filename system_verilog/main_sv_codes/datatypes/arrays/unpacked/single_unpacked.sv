85957669371edd1d277bd257532ebb4405bbeb46
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-06-11 10:09:34 +0530

    Create single_unpacked.sv

---
+++ system_verilog/main_sv_codes/datatypes/arrays/unpacked/single_unpacked.sv
@@ -0,0 +1,19 @@
+module single_unpacked;
+  bit a[3];
+  int b[15];
+  byte c[7];
+  initial begin
+    a='{4,5,0};
+    foreach(b[i])begin
+      b[i]=$urandom_range(6,16);
+    end
+    c='{3,4,5,6,784,1,16};
+    $display("Array elements:%p %p %p",a,b,c);
+    foreach(a[i])
+      $display("Array elements[%0d] : [%0d]",i,a[i]);  
+    foreach(b[i])
+      $display("Array elements[%0d] : [%0d]",i,b[i]);  
+    foreach(c[i])
+      $display("Array elements[%0d] : [%0d]",i,c[i]);  
+  end
+endmodule
