ff075d26fecebfe1a8cc27be16af56e541ad5ed7
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-06-13 17:45:34 +0530

    Update without_super_another_mtd.sv

--- system_verilog/main_sv_codes/oops/super/without_super_another_mtd.sv
+++ system_verilog/main_sv_codes/oops/super/without_super_another_mtd.sv
@@ -1,1 +1,29 @@
-
+//without using super we can assign the parent obj and access but mostly in long code , super is efficient
+//here we can use same name for function in both parent and child this doesnt lead to any overriding as we created seperate object for both parent and child
+class parent;
+  string name;
+  int id;
+  function void display();
+    $display("base class : name:%0s,id=%0d",name,id);
+  endfunction
+endclass
+class child extends parent;
+  function void display();
+    $display("child class : name:%0s,id=%0d",name,id);
+  endfunction
+endclass
+module inheritance_overridng;
+  child c;
+  parent p;
+  initial begin
+    p=new();
+    c=new();
+    c.name="deepa";
+    c.id=16;
+    c.display();
+    p.name="deepavadhana";
+    p.id=36;
+    p.display();
+    end
+endmodule
+    
