758a1b83a658baa7330a8328d24d0dd812c428af
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-06-15 10:31:59 +0530

    Create without_virtual.sv

---
+++ system_verilog/main_sv_codes/oops/virtual_keyword/without_virtual.sv
@@ -0,0 +1,28 @@
+class parent;
+  int id;
+  string name;
+function void display();
+  $display("Parent class:name=%0s,id=%0d",name,id);
+  endfunction
+endclass
+class child extends parent;
+  int id;
+   function void display();
+     $display("Child class: id=%0d",id);
+  endfunction
+endclass
+module without_virtual;
+  child c;
+  parent p;
+  initial begin
+    p=new();
+    c=new();
+    p.name="deepa";
+    c.id=13;
+    p.id=16;
+    c.display();
+    p.display();
+  end
+endmodule
+  
+ 
