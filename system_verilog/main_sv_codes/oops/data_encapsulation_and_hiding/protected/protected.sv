d4a5820cdcabed190e60e68acd659e54d6dc9fb0
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-06-17 11:59:38 +0530

    Update protected.sv

--- system_verilog/main_sv_codes/oops/data_encapsulation_and_hiding/protected/protected.sv
+++ system_verilog/main_sv_codes/oops/data_encapsulation_and_hiding/protected/protected.sv
@@ -1,1 +1,35 @@
 
+
+class parent;
+  int a,b;
+  protected int c;
+  protected int d=6;
+  function int display(int r,int p);
+    c=r+p;
+    return c;
+  endfunction
+  function void displ();
+    $display("value of c=%0d,value of d=%0d",c,d);
+  endfunction
+endclass
+class child extends parent;
+  function void disp();
+    $display("value of c=%0d,value of d=%0d",c,d);
+  endfunction
+endclass
+  module protected_var_ex;
+    initial begin
+      parent p;
+      child c;
+      int e;
+      p=new();
+      c=new();
+     
+     // c.d=8;here we cant access the protected variable of the parent with help of the child object 
+      
+      
+      e=p.display(4,5);
+      p.displ();
+      c.disp();
+    end
+  endmodule
