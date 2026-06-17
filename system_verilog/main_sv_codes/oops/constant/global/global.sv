808a4a57f9b4ddd7bed0f7ae8cbf8359cb181e44
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-06-17 13:01:06 +0530

    Update global.sv

--- system_verilog/main_sv_codes/oops/constant/global/global.sv
+++ system_verilog/main_sv_codes/oops/constant/global/global.sv
@@ -1,1 +1,23 @@
+class transaction;
+  bit [31:0] data;
+  const int id=6;//global constant value declared and locked forever
+  function new();
+    data = 100;
+    //id = 1; //this may bring err 
+  endfunction
+  
+  function void display();
+    $display("data = %0d and id = %0d", data, id);
+  endfunction
+endclass
 
+module class_example;
+  transaction tr;
+  
+  initial begin
+    tr = new();
+    tr.data = 200;
+   // tr.id=9;//this will be err
+    tr.display();
+  end
+endmodule
