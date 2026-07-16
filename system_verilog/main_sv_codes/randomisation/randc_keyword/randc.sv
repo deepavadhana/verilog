ba325e05cd16e318bfdb597cef0cbdd396821e30
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-07-16 12:18:45 +0530

    Create randc.sv

---
+++ system_verilog/main_sv_codes/randomisation/randc_keyword/randc.sv
@@ -0,0 +1,14 @@
+class rand_mtd;
+  randc logic [2:0] a;
+  endclass
+ module rand_top;
+   rand_mtd r;
+   initial begin
+     r=new();
+     $display("randomisation");
+     for(int i=0;i<=10;i++)begin
+       if(r.randomize())
+       $display("Values of a : %b ",r.a);
+   end
+   end
+ endmodule
