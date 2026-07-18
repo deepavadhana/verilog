fbbcb71dffa118809d76bc23bec10facd9719d71
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-07-18 12:04:11 +0530

    Create without_solve_before.sv

---
+++ system_verilog/main_sv_codes/randomisation/Constraints/Solve_before/without_solve_before.sv
@@ -0,0 +1,15 @@
+// Code your design here
+class seq;
+  rand bit [3:0] value;
+  rand bit en;
+  constraint cons {if (en==1) value inside {[10:20]};}
+endclass
+module without_solve_before;
+  seq s=new();
+  initial begin
+    repeat(10)begin
+      s.randomize();
+      $display("without solve before the value =%0d ,en=%0d",s.value,s.en);
+    end
+  end
+endmodule
