07b0697a06c9e495da526d52b5c6c37594aa0ae9
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-07-18 12:42:09 +0530

    Create with_soft.sv

---
+++ system_verilog/main_sv_codes/randomisation/Constraints/Sost_constraint/with_soft.sv
@@ -0,0 +1,18 @@
+
+class seq;
+  rand bit [3:0] val;
+  rand bit [4:0] a;
+  constraint cons { soft val inside{[10:20]};}
+endclass
+module inline_constr();
+  seq s=new();
+  initial begin
+    repeat (8) begin
+      s.randomize();
+      $display("Value=%0d",s.val); 
+      s.randomize with {val inside{[1:6]};};
+      $display("After inline constraint Value=%0d",s.val);
+    end
+  end
+endmodule
+                   
