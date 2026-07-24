22e3630cd3ee12fa65a18d8ef92bc6dc4964f517
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-07-24 10:02:50 +0530

    Create onehot0.sv

---
+++ system_verilog/main_sv_codes/assertion/SV_methods/onehot0/onehot0.sv
@@ -0,0 +1,32 @@
+// Code your testbench here
+// or browse Examples
+// Code your testbench here
+// or browse Examples
+module seq;
+  bit clk,a;
+  logic [3:0] b;
+  always #5 clk=~clk;
+  initial begin
+    clk=0;
+    #5; a=1;b=4'b1000;
+    #5; a=0;b=4'b1110;
+    #5; a=1;b=4'b1011;
+    #5; a=0;b=4'b1001;
+    #5; a=1;b=4'b0000;
+    #5; a=1;b=4'b0001;
+    #5; a=0;b=4'b1100;
+    #5; a=1;b=4'b0100;
+    #5; a=0;b=4'b1000;
+    #5; a=1;b=4'b1010;
+    #5; a=1;b=4'b1001;
+    #100; $finish;
+  end
+  property pr;
+    @(posedge clk) a|->$onehot0(b);
+  endproperty
+  assert property(pr)
+    $info("Pass");
+    else
+      $info("Fail");
+    endmodule
+    
