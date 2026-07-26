76311469204a7ce0152236a844e31aa55c11d5db
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-07-26 17:49:35 +0530

    Create seq_of_trans.sv

---
+++ system_verilog/main_sv_codes/functional_coverage/bins/transition_bins/seq_of_trans/seq_of_trans.sv
@@ -0,0 +1,22 @@
+
+module seq_trans;
+  bit [3:0] a;
+  bit [2:0] arr[$]='{1,2,3,4,1,1,2,4,3,5};
+  covergroup cg;
+    a: coverpoint a{
+      bins b1=(1=>2=>3);
+      bins b2=(1=>2=>4);
+      //use bracket for trans bins
+    }
+  endgroup
+  cg c;
+  initial begin
+    c=new();
+    foreach (arr[i])begin
+      a=arr[i];
+      c.sample();
+      $display("values=%0b coverage_percent=%.2f",a,c.get_inst_coverage());
+    end
+  end
+endmodule
+    
