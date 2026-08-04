989396272540dcd2c1be9d8205905613cbc598cf
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-08-04 12:42:11 +0530

    Create try_get.sv

---
+++ system_verilog/main_sv_codes/Interprocess_methods/mailbox/mailbox_methods/try_get_mtd/try_get.sv
@@ -0,0 +1,52 @@
+
+class A;
+  int a;
+  mailbox mb;
+  function new(mailbox mb);
+    this.mb=mb;
+  endfunction
+  task load();
+    for(int i=0;i<3;i++)begin
+        a++;
+      mb.put(a);
+      $display("Mailbox created ");
+      $display("transmission of data =%0d",a);
+      
+      end
+    $display("No of msgs :%0d ",mb.num());
+      
+  endtask
+endclass
+class B;
+  int a;
+  mailbox mb;
+  function new(mailbox mb);
+    this.mb=mb;
+  endfunction
+  task trans();
+    begin
+      if(mb.try_get(a)==1)begin
+        mb.get(a);
+      $display("Recieving of data =%0d",a);
+      $display("No of msgs :%0d ",mb.num());
+      end
+      else begin
+        $display("NO recieve data");
+      end
+    end
+      endtask
+endclass
+  module try_get_mtd;
+    A a1;
+    B b1;
+    mailbox m=new();
+    initial begin
+      a1=new(m);
+      b1=new(m);
+      repeat(10) begin
+      a1.load();
+      b1.trans();
+      end
+    end
+  endmodule
+               
