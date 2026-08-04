cdfd5c302358c8f24682bac8c58a008d65f4a416
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-08-04 12:50:30 +0530

    Create try_peek.sv

---
+++ system_verilog/main_sv_codes/Interprocess_methods/mailbox/mailbox_methods/try_peek_mtd/try_peek.sv
@@ -0,0 +1,55 @@
+
+class A;
+  int a;
+  mailbox mb;
+  function new(mailbox mb);
+    this.mb=mb;
+  endfunction
+  task load();
+    for(int i=0;i<10;i++)begin
+        a++;
+      mb.put(a);
+      $display("Mailbox created ");
+      $display("transmission of data =%0d",a);
+      
+      end
+    $display("No of msgs :%0d ",mb.num());
+      
+  endtask
+endclass
+class B;
+  int a;
+  mailbox mb;
+  function new(mailbox mb);
+    this.mb=mb;
+  endfunction
+  task trans();
+    for(int i=0;i<10;i++)begin
+      
+      if(mb.try_peek(a))
+      $display("Peeking of data =%0d",a);
+      else
+        $display("No Peeking of data");
+
+      end
+    begin
+      mb.get(a);
+      $display("Recieving of data =%0d",a);
+      $display("No of msgs :%0d ",mb.num());
+      end
+      endtask
+endclass
+  module try_peek_mtd;
+    A a1;
+    B b1;
+    mailbox m=new();
+    initial begin
+      a1=new(m);
+      b1=new(m);
+      repeat(10) begin
+      a1.load();
+      b1.trans();
+      end
+    end
+  endmodule
+               
