5ba029eabc836f3d88e70ea0e7daf3c40018432b
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-08-13 10:58:03 +0530

    Update sema.sv

--- system_verilog/main_sv_codes/Interprocess_methods/semaphore/semaphore_2/sema.sv
+++ system_verilog/main_sv_codes/Interprocess_methods/semaphore/semaphore_2/sema.sv
@@ -1,1 +1,17 @@
-
+module semaphore_example2;
+  semaphore sem =new(6);
+  initial begin
+    sem.get(4);
+    $display("first process:thread in the first process:%0t",$time);
+    #5;
+    sem.put(2);
+    $display("first process:thread in the first process:%0t",$time);
+  end
+   initial begin
+     sem.get(5);//this process will never start as the first process gets only 4 keys so if the keys are unavailable it will not start ,hence the display statement of 
+     $display("second process:thread in the second process:%0t",$time);
+    #15;
+     sem.put(4);
+     $display("second process:thread in the second process:%0t",$time);
+  end
+endmodule
