d0b6207c83c349e3650441cf3ed61aaca4378074
deepavadhana <146067921+deepavadhana@users.noreply.github.com>
2026-08-13 09:36:34 +0530

    Update sema.sv

--- system_verilog/main_sv_codes/Interprocess_methods/semaphore/semaphore_1/sema.sv
+++ system_verilog/main_sv_codes/Interprocess_methods/semaphore/semaphore_1/sema.sv
@@ -1,1 +1,18 @@
-
+module semaphore_example_1;
+  semaphore sem=new(6);
+  initial begin
+    sem.get(3);
+    $display("SEmaphore key for the process 1 at the time:%0t",$time);
+    #5;
+    sem.put(3);
+    $display(" Recieved SEmaphore key for the process 1 at the time:%0t",$time);
+  end
+  initial begin
+    #10;
+    sem.get(2);
+    $display("SEmaphore key for the process 2 at the time:%0t",$time);
+    #3;
+    sem.put(2);
+    $display(" Recieved SEmaphore key for the process 2 at the time:%0t",$time);
+  end
+endmodule
